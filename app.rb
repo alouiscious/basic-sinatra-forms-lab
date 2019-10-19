59504a5bde59c4d794781c134743776a1febd813
Alouiscious Media <info@reneesoul.com>
2019-10-19 05:07:56 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,16 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+  get '/newteam' do
+    erb :newteam
+  end
 
+  post '/team' do
+    team_specs = ''
+    binding.pry
+    team_specs = params["strings"]
+
+    erb :team
+  end
 
 end
